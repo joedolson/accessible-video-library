207eacc28a015d46b3f0d82dc61c0e24838b7b91
joedolson <design@joedolson.com>
2015-01-24 13:36:10 -0600

    Add files

---
+++ js/avl-mediaelement.js
@@ -0,0 +1,16 @@
+(function ($) {
+	// add mime-type aliases to MediaElement plugin support
+	mejs.plugins.silverlight[0].types.push('video/x-ms-wmv');
+	mejs.plugins.silverlight[0].types.push('audio/x-ms-wma');
+
+	$(function () {
+		var settings = {};
+
+		if ( typeof _avlmejsSettings !== 'undefined' )
+			settings.pluginPath = _avlmejsSettings.pluginPath;
+			settings.alwaysShowControls = _avlmejsSettings.alwaysShowControls;
+			
+		$('.wp-audio-shortcode, .wp-video-shortcode').mediaelementplayer( settings );
+	});
+
+}(jQuery));
